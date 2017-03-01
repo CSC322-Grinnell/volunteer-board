ebf60277c16b6ef468d931b18410b6ca6981c864
Zoe Grubbs <grubbs.zoe@gmail.com>
2017-03-01 20:53:27 +0000

    Generated event model

---
+++ db/migrate/20170301202916_create_events.rb
@@ -0,0 +1,10 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.time :date_time
+
+      t.timestamps
+    end
+  end
+end
