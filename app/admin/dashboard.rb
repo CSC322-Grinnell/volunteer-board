ddb8f1648b445211b25864cce399d8100052ad63
Benjamin Wong <wongbenj@grinnell.edu>
2017-03-13 18:58:32 +0000

    Created organization and user file for admin but need appropriate models

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,30 @@
 ActiveAdmin.register_page "Dashboard" do
-
+  
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
+  
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+      #div class: "blank_slate_container", id: "dashboard_default_message" do
+        #span class: "blank_slate" do
+          #span I18n.t("active_admin.dashboard_welcome.welcome")
+          #small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        #end
+      #end
+    
+    # Admin Users
+    def approve_org(org)
+      org.approved = true
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    
+    @organization = Organization.where(approved: false)
+    columns do
+      column do
+        panel 'Unapproved Organizations' do
+          @organization.each do |org|
+            # How organization shows up
+            li link_to "View #{org.name} (#{org.email})", adminorg_path(org.id)
+          end
+        end
+      end
+    end # content
+  end
 end
---
+++ app/admin/organization.rb
@@ -0,0 +1,50 @@
+ActiveAdmin.register Organization do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  config.comments = false
+  index do
+    column :name
+    column :email
+    column :contact_name
+    column :mission
+    column :website
+    column :approved
+    actions
+  end
+
+  filter :email
+
+  form do |f|
+    
+    f.inputs 'Admin Details' do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+    
+  end
+  
+  show do
+    attributes_table :name, :email, :approved, :contact_name, :address, :city, :state, :zipcode, :telephone, :mission, :website
+  end
+  
+  # Adds "Approve" button, if organization isn't approved yet
+  # !!! Needs testing !!!
+  # Useful pages:
+  # https://www.google.com/search?q=edit+active+admin+show+actions
+  # https://github.com/activeadmin/activeadmin/issues/341
+  # http://activeadmin.info/docs/8-custom-actions.html
+  action_item :approve, only: :show do #only in the show page!
+    if !Organization.find(params[:id]).approved? then
+      link_to "Approve", approve_org_path(params[:id])
+    end
+  end
+  
+  action_item :view_as_user, only: :show do
+    link_to "View As User", org_path(params[:id])
+  end
+  
+end
+
