2495c8ea41ea3ea2fcdf0b1cbb23d23e37c3bdc1
Tyler Williams <williams13@grinnell.edu>
2017-04-03 19:09:56 +0000

    Adding Test

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,14 @@
 class Organization < ApplicationRecord
-    has_many :events
+   validates :name, presence: true
+   validates :address, presence: true
+   validates :city, presence: true
+   validates :state, presence: true
+   validates :zip, presence: true
+   validates :phone, presence: true
+   validates :email, presence: true
+   validates :mission, presence: true
+   validates :website, presence: true
+   validates :password, presence: true
+   validates :confirm, presence: true
+   
 end
--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,7 +1,67 @@
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @organization = Organization.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @organization.valid?
+  end
+  
+  test "name should be present" do
+    @organization.name = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "address should be present" do
+    @organization.address = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "city should be present" do
+    @organization.city = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "state should be present" do
+    @organization.state = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "zip should be present" do
+    @organization.zip = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "phone should be present" do
+    @organization.phone = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "emial should be present" do
+    @organization.email = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "mission should be present" do
+    @organization.mission = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "website should be present" do
+    @organization.website = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "password should be present" do
+    @organization.password = "     "
+    assert_not @organization.valid?
+  end
+  
+  test "confirm should be present" do
+    @organization.confirm = "     "
+    assert_not @organization.valid?
+  end
 end
