906d75f1daf1e0837e6e6e0379a1f850e240f1ee
grosoon <bourgeoi1@grinnell.edu>
2017-04-19 19:42:42 +0000

    added profile page for users. currently broken

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     get 'sign_up', to: 'users/registrations#new'
     get 'sign_out', to: 'users/sessions#destroy'
   end
+  
+  get 'users/:id', to: 'users/profile#show'
 
 
   devise_for :admin_users, ActiveAdmin::Devise.config
