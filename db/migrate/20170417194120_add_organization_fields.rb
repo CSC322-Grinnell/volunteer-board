d041ebe1b722d5710f24e91bba0ae42c8d0ba9f6
grosoon <bourgeoi1@grinnell.edu>
2017-04-19 18:16:03 +0000

    ready to push

---
+++ db/migrate/20170417194120_add_organization_fields.rb
@@ -0,0 +1,13 @@
+class AddOrganizationFields < ActiveRecord::Migration[5.0]
+  def change
+    add_column :organizations, :name, :string
+    add_column :organizations, :address, :string
+    add_column :organizations, :city, :string
+    add_column :organizations, :state, :string
+    add_column :organizations, :zip, :string
+    add_column :organizations, :phone, :integer
+    add_column :organizations, :mission, :string
+    add_column :organizations, :website, :string
+    
+  end
+end
