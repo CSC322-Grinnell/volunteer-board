ff1e6a26102e3b80bce39ce5cb7845acd069ffc4
Benjamin Wong <wongbenj@grinnell.edu>
2017-04-26 18:02:01 +0000

    Login bug fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin1@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'testadmin@example.com', password: 'password', password_confirmation: 'password')
