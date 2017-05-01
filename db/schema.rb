8d04c0551d99c2abd135ac1bd18eb32e824bbbfc
Tyler Williams <williams13@grinnell.edu>
2017-05-01 19:35:59 +0000

    cleaning profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426183214) do
+ActiveRecord::Schema.define(version: 20170428193438) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170426183214) do
     t.string   "contact_email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "org_num"
     t.integer  "organization_id"
   end
 
@@ -62,6 +63,10 @@ ActiveRecord::Schema.define(version: 20170426183214) do
     t.integer "event_id", null: false
   end
 
+  create_table "organization_events", force: :cascade do |t|
+    t.integer "organization"
+    t.integer "event"
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
