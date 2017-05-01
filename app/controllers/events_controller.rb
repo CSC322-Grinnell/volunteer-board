5b4d9005181bcc87932d7a663d811f0895c4583a
grosoon <bourgeoi1@grinnell.edu>
2017-05-01 18:19:33 +0000

    added buttons to create and destroy relationships between events and users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,20 @@
 class EventsController < InheritedResources::Base
+  
+  def sign_up
+    @event = Event.find(params[:id])
+    if user_signed_in?
+      current_user.events << @event
+    end
+    redirect_to action: "show", id: @event.id
+  end
+  
+  def deregister
+    @event = Event.find(params[:id])
+    if user_signed_in?
+      current_user.events.delete(@event)
+    end
+    redirect_to action: "show", id: @event.id
+  end
 
   private
 
@@ -10,12 +26,6 @@ class EventsController < InheritedResources::Base
     #   puts "*** Showing"
     # end
     
-    def sign_up
-      @event = Event.find(params[:id])
-      if user_signed_in?
-        current_user.events << @event
-      end
-      redirect_to action: "show", id: @event.id
-    end
+
 end
 
