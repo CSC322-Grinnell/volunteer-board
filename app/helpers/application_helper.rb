2862cbf28dfdf1246b7c41d1435368936b0f9c15
Zoe Grubbs <grubbs.zoe@gmail.com>
2017-05-08 18:25:55 +0000

    added time select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def my_hour_select
+        select_tag('hour',options_for_select([1,2,3,4,5,6,7,8,9,10,11,12]))
+    end
+    
+    def my_minute_select
+        l = ["00","01","02","03","04","05","06","07","08","09"] + [*10..59].map{ |n| n.to_s}
+        select_tag('minute',options_for_select(l))
+    end
+    
+    def my_ampm_select
+        select_tag('ampm',options_for_select([["am",0],["pm",1]]))
+    end
 end
