99dd2fef58f4815ec2be35985fd1a1bf05e674d7
George Ge <gezhi@grinnell.edu>
2017-11-14 16:45:27 +0000

    Event creation no longer errors out, but still doesn't work because dates don't get stored

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,40 +28,50 @@ class EventsController < InheritedResources::Base
   end
 
   def create
-    event_params
     if !organization_signed_in?
       return
     end
 
-    st = compute_date(:start_time)
-    et = compute_date(:end_time)
+    start_hour = params[:start_hour].to_i + params[:start_ampm].to_i*12
+    
+    if event_params["start_date(2i)"].length == 1
+      start_month = "0" + event_params["start_date(2i)"]
+    else
+      start_month = event_params["start_date(2i)"]
+    end
+    
+    start_time = event_params["start_date(1i)"].to_s + start_month.to_s + event_params["start_date(3i)"].to_s + "T" + 
+      start_hour.to_s + ":" + params[:start_minute].to_s + "-0500"
+    st = DateTime.strptime(start_time, '%Y%m%dT%H:%M%Z')
+    
+    end_hour = params[:end_hour].to_i + params[:end_ampm].to_i*12
+    
+    if event_params["end_date(2i)"].length == 1
+      end_month = "0" + event_params["end_date(2i)"]
+    else
+      end_month = event_params["end_date(2i)"]
+    end
+    
+    end_time = event_params["end_date(1i)"].to_s + end_month.to_s + event_params["end_date(3i)"].to_s + "T" +
+      end_hour.to_s + ":" + params[:end_minute].to_s + "-0500"
+    et = DateTime.strptime(end_time, '%Y%m%dT%H:%M%Z')
     
     event_info = { :start_time => st, :end_time => et, :name => event_params[:name],
       :description => event_params[:description], :num_vols => event_params[:num_vols], :location => event_params[:location],
       :contact_phone => event_params[:contact_phone], :contact_email => event_params[:contact_email]}
       
     @event = current_organization.events.create!(event_info)
-
-#    @event = current_organization.events.new(event_info)
     
-    
-#    @event.save!
     redirect_to event_url(@event)
   end
 
 
   private
 
-    def compute_date(date)
-      if event_params[date] == nil
-        return nil
-      end
-      
-      return DateTime.strptime(event_params[date], '%Y-%m-%d %H:%M:%S %z')
-    end
-
     def event_params
-      params.require(:event).permit(:name, :description, :start_time, :end_time,
-      :num_vols, :location, :contact_phone, :contact_email, :hour, :minute, :ampm, :start_date, :end_date, :volunteer_count)
+      params.require(:event).permit(:name, :description, :num_vols, :location,
+        :contact_phone, :contact_email, :end_hour, :end_minute, :end_ampm,
+        :start_hour, :start_minute, :start_ampm, :start_date, :end_date, 
+        :volunteer_count)
     end
 end
