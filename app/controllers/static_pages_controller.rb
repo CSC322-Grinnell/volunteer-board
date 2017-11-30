9fd75a3c1d33c39647fdb1f7dd9c8322c681c385
Beatriz <hercehag@grinnell.edu>
2017-11-30 02:31:47 +0000

    removed merge conflicts + syntax errors

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,28 +1,27 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
-  
-<<<<<<< HEAD
-  def signin_options
-  end
-  
-  def signup_options
-=======
-  def faq
->>>>>>> 568ae37ee63579145c79dcc92f56279b6b2bd92c
-  end
-  
-  # !!! Needs Testing !!!
-  def approve_org
-    @organization = Organization.find(params[:id])
-    if admin_user_signed_in?
-      @organization.approved = true
-      if @organization.save
-        flash[:notice] = "#{@organization.name} approved"
-      else 
-        
-      end
-      redirect_to '/admin'
-    end
-  end
+def home
 end
+
+def signin_options
+end
+
+def signup_options
+end
+
+def faq
+end
+
+# !!! Needs Testing !!!
+	def approve_org
+@organization = Organization.find(params[:id])
+	if admin_user_signed_in?
+	@organization.approved = true
+	if @organization.save
+	flash[:notice] = "#{@organization.name} approved"
+	else 
+
+	end
+	redirect_to '/admin'
+	end
+	end
+	end
