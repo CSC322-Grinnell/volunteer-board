01f453abd4f13179659170700c9f0e08658a3454
Zebediah Figura <z.figura12@gmail.com>
2019-03-10 20:14:06 -0500

    controllers/organizations/sessions_controller: Remove unused configure_sign_in_params() method.
    This was added in b7c5916e6. As far as I can tell, this was
    misguided. The intended pattern is to also uncomment the top call to
    before_action(), whereupon configure_sign_in_params() would be called
    as a hook. However, that was not done. Since we do the actual
    authentication inside of the Organizations model, this was unnecessary
    anyway.
    
    Beyond that it's also not clear to me how this was supposed to have
    worked: as I understand (and the documentation is as always not quite
    clear), Devise parameter sanitation is for parameters passed in
    through the POST request. Therefore this code would have had the
    effect of allowing an "approved" parameter to be posted by the user,
    which is several steps away from what we actually want to have happen.

--- app/controllers/organizations/sessions_controller.rb
+++ app/controllers/organizations/sessions_controller.rb
@@ -1,6 +1,4 @@
 class Organizations::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
   # GET /resource/sign_in
   # def new
   #   super
@@ -15,11 +13,4 @@ class Organizations::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_in_params
-     devise_parameter_sanitizer.permit(:sign_in, keys: [:approved])
-   end
 end
