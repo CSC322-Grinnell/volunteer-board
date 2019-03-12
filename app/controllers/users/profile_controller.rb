d9c3129a24cc97d7d13bad7ab0fda5bd67167d14
Zebediah Figura <z.figura12@gmail.com>
2019-03-11 23:07:29 -0500

    users/profile: Get rid of unnecessary events local variable.

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -1,18 +1,17 @@
 class Users::ProfileController < ApplicationController
 #before_filter :authenticate_user!
 
-  #grabs the current users id and then renders the profile page for them. 
+  #grabs the current users id and then renders the profile page for them.
   def show
     @user = User.find(params[:id])
-    @events = @user.events
 
     render "show"
   end
-  
+
   #gets a list of all users and then renders them in a list through users/profile/list.html.erb
-  def list 
+  def list
     @users = User.all
-    
+
     render "list"
   end
 
